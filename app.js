505bec563f43876b91ca3afb29fa07c075cf3ae5
coldboy002 <coldboy002@gmail.com>
2020-07-11 02:26:38 +0700

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
 
-var app = express();
+require("dotenv").config();
+
+const app = express();
 //Import the mongoose module
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 //Set up default mongoose connection
-// var mongoDB = 'mongodb://127.0.0.1/my_database';
-var mongoDB =
-  "mongodb+srv://trongthuy94:vtt260494@cluster0.akkjp.azure.mongodb.net/book-app?retryWrites=true&w=majority";
+const mongoDB =
+  "mongodb+srv://" +
+  process.env.MONGODB_USERNAME +
+  ":" +
+  process.env.MONGODB_PASSWORD +
+  "@cluster0.akkjp.azure.mongodb.net/book-app?retryWrites=true&w=majority";
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Get the default connection
-var db = mongoose.connection;
+const db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
@@ -36,7 +41,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
